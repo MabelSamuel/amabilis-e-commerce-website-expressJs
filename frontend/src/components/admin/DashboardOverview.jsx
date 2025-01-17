76da83cb8969106c91c0102427a1260503ccd67a
Mabel Samuel <mapleberrysamuel@gmail.com>
2025-01-17 16:48:19 +0100

    moved all data to lib folder

--- frontend/src/components/admin/DashboardOverview.jsx
+++ frontend/src/components/admin/DashboardOverview.jsx
@@ -5,80 +5,9 @@ import OverviewCard from "./ui/OverviewCard";
 import OverviewRectangleCard from "./ui/OverviewRectangleCard";
 import Table from "../table/Table";
 
-import { LuPackage } from "react-icons/lu";
-import { HiOutlineShoppingCart } from "react-icons/hi";
-import { IoCardOutline } from "react-icons/io5";
-import { BsTruck } from "react-icons/bs";
-import { GrStatusGood } from "react-icons/gr";
-import { FaRotate } from "react-icons/fa6";
-
-import { columns, data } from "../../data/dashboard-overview"
+import { columns, data } from "../../lib/dashboard/dashboard-overview";
 
 const DashboardOverview = () => {
-  const cardItems = [
-    {
-      icon: <LuPackage size={50} />,
-      label: "Today Orders",
-      price: 5000.81,
-      class: "bg-teal-100",
-    },
-    {
-      icon: <LuPackage size={50} />,
-      label: "Yesterday Orders",
-      price: 5050.18,
-      class: "bg-amber-100",
-    },
-    {
-      icon: <HiOutlineShoppingCart size={50} />,
-      label: "This Month",
-      price: 15000.81,
-      class: "bg-blue-100",
-    },
-    {
-      icon: <IoCardOutline size={50} />,
-      label: "Last Month",
-      price: 15080.81,
-      class: "bg-pink-200",
-    },
-    {
-      icon: <IoCardOutline size={50} />,
-      label: "All-time Sales",
-      price: 589000.81,
-      class: "bg-emerald-200",
-    },
-  ];
-
-  const rectangleItems = [
-    {
-      bgColor: "bg-red-200",
-      textColor: "text-red-500",
-      icon: <HiOutlineShoppingCart size={20} />,
-      title: "Total Order",
-      count: 518,
-    },
-    {
-      bgColor: "bg-pink-200",
-      textColor: "text-red-400",
-      icon: <FaRotate size={20} />,
-      title: "Orders Pending",
-      count: 108,
-    },
-    {
-      bgColor: "bg-emerald-200",
-      textColor: "text-emerald-500",
-      icon: <BsTruck size={20} />,
-      title: "Orders Processing",
-      count: 518,
-    },
-    {
-      bgColor: "bg-emerald-200",
-      textColor: "text-emerald-500",
-      icon: <GrStatusGood size={20} />,
-      title: "Orders Delivered",
-      count: 490,
-    },
-  ];
-
   return (
     <div className="space-y-10">
       <DashboardTitle title="Dashboard Overview" />
--- frontend/src/lib/dashboard/dashboard-overview.js
+++ frontend/src/lib/dashboard/dashboard-overview.js
@@ -1,3 +1,10 @@
+import { LuPackage } from "react-icons/lu";
+import { HiOutlineShoppingCart } from "react-icons/hi";
+import { IoCardOutline } from "react-icons/io5";
+import { BsTruck } from "react-icons/bs";
+import { GrStatusGood } from "react-icons/gr";
+import { FaRotate } from "react-icons/fa6";
+
 export const columns = [
   { header: "Order ID", accessor: "id" },
   { header: "Order Date", accessor: "orderDate" },
@@ -179,5 +186,69 @@ export const data = [
     orderCost: "$1,000.00",
     status: "Delivered",
     email: "tony.s@example.com",
+  },
+];
+
+export const cardItems = [
+  {
+    icon: <LuPackage size={50} />,
+    label: "Today Orders",
+    price: 5000.81,
+    class: "bg-teal-100",
+  },
+  {
+    icon: <LuPackage size={50} />,
+    label: "Yesterday Orders",
+    price: 5050.18,
+    class: "bg-amber-100",
+  },
+  {
+    icon: <HiOutlineShoppingCart size={50} />,
+    label: "This Month",
+    price: 15000.81,
+    class: "bg-blue-100",
+  },
+  {
+    icon: <IoCardOutline size={50} />,
+    label: "Last Month",
+    price: 15080.81,
+    class: "bg-pink-200",
+  },
+  {
+    icon: <IoCardOutline size={50} />,
+    label: "All-time Sales",
+    price: 589000.81,
+    class: "bg-emerald-200",
+  },
+];
+
+export const rectangleItems = [
+  {
+    bgColor: "bg-red-200",
+    textColor: "text-red-500",
+    icon: <HiOutlineShoppingCart size={20} />,
+    title: "Total Order",
+    count: 518,
+  },
+  {
+    bgColor: "bg-pink-200",
+    textColor: "text-red-400",
+    icon: <FaRotate size={20} />,
+    title: "Orders Pending",
+    count: 108,
+  },
+  {
+    bgColor: "bg-emerald-200",
+    textColor: "text-emerald-500",
+    icon: <BsTruck size={20} />,
+    title: "Orders Processing",
+    count: 518,
+  },
+  {
+    bgColor: "bg-emerald-200",
+    textColor: "text-emerald-500",
+    icon: <GrStatusGood size={20} />,
+    title: "Orders Delivered",
+    count: 490,
   },
 ];
