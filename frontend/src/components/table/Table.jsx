fcb05052e615cfa3c0fcea930275979e92f89ebf
Mabel Samuel <mapleberrysamuel@gmail.com>
2025-02-16 20:32:39 +0100

    changed table color

--- frontend/src/components/table/Table.jsx
+++ frontend/src/components/table/Table.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Table = ({ columns, data }) => {
   return (
-    <table className="min-w-full border-collapse border border-gray-300">
+    <table className="min-w-full border-collapse border bg-white border-gray-400">
       <thead>
         <tr>
           {columns.map((col) => (
             <th
               key={col.accessor}
-              className="px-4 py-2 border border-gray-300 bg-gray-400 text-left"
+              className="px-4 py-2 border border-gray-300 bg-gray-200 text-left"
             >
               {col.header}
             </th>
